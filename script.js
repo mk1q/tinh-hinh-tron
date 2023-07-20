f15cd53c6056b5c81ac62eb063152b8a8e36870a
ngo ba kha <anhemdaica123@gmail.com>
2023-07-20 13:26:57 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+//Tính chu vi và diện tích hình tròn
+
+let R = prompt('hãy nhập bán kính'); parseInt(R);
+let pi= 3.14 ;
+
+let arrea= pi*2*R;
+let S= R^2*pi ;
+
+document.writeln("Chu vi hình tròn là: " + arrea );
+document.write('<br/>');
+document.writeln("Diện tích hình tròn là: " + S );
+
+
